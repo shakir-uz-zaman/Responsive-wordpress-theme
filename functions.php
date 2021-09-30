3433457f96be24d7ca5b4bd6be490523ddef1c02
shakir-uz-zaman <shakir.uz.zaman.shuvo@gmail.com>
2021-09-30 10:43:58 +0600

    comment section added

--- functions.php
+++ functions.php
@@ -1,6 +1,23 @@
 <?php
 
-//Add cs files
+function devwp_theme_support(){
+    add_theme_support("title-tag");
+    add_theme_support("custom-logo");
+}
+
+add_action('after_setup_theme','devwp_theme_support');
+
+function devwp_menus(){
+
+    $locations = array(
+        'primary' => "Desktop Primary Left Sidebar",
+        'footer' => "Footer Menu Items"
+    );
+    register_nav_menus( $locations );
+}
+add_action("init","devwp_menus");
+
+// Add cs files
 function devwp_register_styles(){
     $version = wp_get_theme()->get('Version');
     wp_enqueue_style('devwp-custom', get_template_directory_uri() . "/style.css", array('devwp-bootstrap'), $version, 'all');
@@ -10,17 +27,29 @@ function devwp_register_styles(){
 
 add_action( 'wp_enqueue_scripts', 'devwp_register_styles');
 
-//Add js files
+// Add js files
 function devwp_register_scripts(){
 
-    wp_enqueue_script('devep-jquery',"https://code.jquery.com/jquery-3.4.1.slim.min.js", array() , "3.4.1", true  );
-    wp_enqueue_script('devep-popper',"https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js", array(), "1.16.0", true   );
-    wp_enqueue_script('devep-bootstrap',"https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js", array() ,"4.4.1" , true  );
-    wp_enqueue_script('devep-custom',get_template_directory_uri( )."/assets/js/main.js", array() , "1.0 ",  true );
+    wp_enqueue_script('devwp-jquery',"https://code.jquery.com/jquery-3.4.1.slim.min.js", array() , "3.4.1", true  );
+    wp_enqueue_script('devwp-popper',"https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js", array(), "1.16.0", true   );
+    wp_enqueue_script('devwp-bootstrap',"https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js", array() ,"4.4.1" , true  );
+    wp_enqueue_script('devwp-main',get_template_directory_uri( )."/assets/js/main.js", array() , "1.0 ",  true );
 }
 
 add_action( 'wp_enqueue_scripts', 'devwp_register_scripts');
 
+add_filter( 'comments_number', 'wporg_com_num', 10, 2 );
+function wporg_com_num ( $out, $num ) { // Two parameter as in filter described
+    if ( 0 === $num ) {
+        $out = '0 Comments'; // If No comments
+    } elseif ( 1 === $num ) {
+        $out = '1 Comment'; // If 1 comment
+    } else {
+        $out = $num . ' Comments'; // More than 1 comment
+    }
+
+    return $out;
+}
 
 
 
---
+++ template-parts/content-article.php
@@ -0,0 +1,18 @@
+<div class="container">
+     <header class="content-header">
+          <div class="meta mb-3"><span class="date">Published <?php the_date(); ?></span>
+          <?php
+               the_tags('<span class="tag"><i class="fa fa-tag"></i>','</span><span class="tag"><i class="fa fa-tag"></i>','</span>');
+          ?>
+
+          <span class="comment">  <a id="commenets_number" href="#comments"><i class='fa fa-comment'></i><?php  comments_number(); ?>
+         </a></span></div>
+     </header>
+     <?php
+          the_content();
+     ?>
+     <?php
+          comments_template();
+     ?>
+
+</div>
