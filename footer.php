d613e3a05d73a7792aa212b50ced997737d38580
Shakir <shakir@as-MacBook-Pro.local>
2021-11-03 11:27:32 +0600

    complete theme

---
+++ footer.php
@@ -0,0 +1,26 @@
+<!-- <footer class="footer text-center py-2 theme-bg-dark">
+
+    <?php// dynamic_sidebar('footer-1'); ?>
+
+</footer>
+
+</div> -->
+
+<!-- <?php // wp_footer(); ?>
+
+</body>
+
+</html> -->
+
+<?php
+	wp_footer();
+?>
+<footer class="footer text-center py-2 theme-bg-dark">
+
+<p class="copyright"><a href="#">Them@2021</a></p>
+
+</footer>
+
+</div>
+</body>
+</html>
